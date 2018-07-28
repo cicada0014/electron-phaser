6ec5677a9505becdd3417b5179dfa60181934270
Youngtae <cicada0014@gmail.com>
2018-07-28 11:37:04 +0900

    음... 페이저 모듈 자체가 브라우저 window 객체를 사용하고있다. 일렉트론 브라우저 윈도를 요녀석한테 넣을 수만 있다면 가능하지 않을까 생각햇는데... 생각보다 더 복잡한듯하다. 좀 더 시도를 해바야겠다. 오늘은 여기까지

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import { app, App, BrowserWindow } from 'electron';
-
+import * as Phaser from 'phaser';
 
 
 class ElectronPhaser {
 
     private app: App
+    private mainWindow: BrowserWindow;
+    gameConfig;
+    phaserGame
 
     constructor() {
         this.app = app;
@@ -15,8 +18,47 @@ class ElectronPhaser {
 
         this.app.on('ready', event => {
             console.log('ready')
+            this.mainWindow = new BrowserWindow({
+                width: 600,
+                height: 800
+            });
+            // this.mainWindow.loadURL('https://analytics.forceteller.com')
+
+            (window as any) = this.mainWindow
+            this.gameConfig = {
+                type: Phaser.CANVAS,
+                width: 2000,
+                height: 1200,
+                zoom: 0.6,
+                physics: {
+                    default: 'arcade',
+                    arcade: {
+                        gravity: {
+                            y: 200
+                        }
+                    }
+
+                },
+
+                parent: 'game-canvas',
+                scene: [
+                    // this.menuScene,
+                    // this.stageScene
+                ]
+            }
+            this.phaserGame = new Phaser.Game(this.gameConfig)
+
+
         })
         this.app.on('before-quit', event => {
+
+        })
+        this.app.on('window-all-closed', event => {
+            this.app.quit();
+        })
+        this.app.on('activate', (event, hasVisibleWindows) => {
+            // Mac OS에서만 발생되는 이벤트임.
+
 
         })
     }
